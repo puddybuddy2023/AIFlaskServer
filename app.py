9488fab3e98951b734a87d51ba0277cf265f50a0
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-12-04 01:23:09 +0900

    모듈 생성

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def test():
     return "hello flask"
 
+@app.route('/petsnal_color')
+def process_image():
+    image_url = request.args.get('image_url')  # 이미지 URL을 파라미터로 받기
+    prefer_id = request.args.get('prefer_id')
+
+    return "Image processing completed!"
+    
+
 if __name__ == '__main__':
     #  FLASK_APP=app.py flask run 으로 실행
     app.run(port=5000,host="0.0.0.0")
