7ce31ae329410028c38e1f094d35915bc7f51c05
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-12-04 18:50:32 +0900

    gitignore

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from modules.petsnal_colors import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'hello'
+
+@app.route('/url_image')
+def url_image_test():
+    image = process_image_from_url("https://puddybuddybucket.s3.amazonaws.com/images/ac6d4434-ac3a-435b-b8aa-31a5989da144_%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%86%B71.png-1.png")
+    return "good"
+
+if __name__ == '__main__':
+    app.run(debug=True)
